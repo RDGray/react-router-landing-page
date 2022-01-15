1bb4ee559807fa513c311c00b56a5cefd3631c65
RDGray <70549101+RDGray@users.noreply.github.com>
2022-01-15 03:39:46 +0300

    home page

--- src/components/Video.js
+++ src/components/Video.js
@@ -13,14 +13,15 @@ function video() {
       <div className="content">
         <h1> Galaxy Travel</h1>
         <p>World's first civilian space travel.</p>
-      </div>
-      <div>
-        <Link to="/training" className="btn">
-          Training
-        </Link>
-        <Link to="/contact" className="btn btn-light">
-          Launch
-        </Link>
+
+        <div>
+          <Link to="/training" className="btn">
+            Training
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Launch
+          </Link>
+        </div>
       </div>
     </div>
   );
